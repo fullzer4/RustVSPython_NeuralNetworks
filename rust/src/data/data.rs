6a11dba1027a8df8ef07610b841db6d4dce85b6c
Gabriel Pelizzaro Pereira <gabrielpelizzaro@gmail.com>
2023-05-31 01:32:50 +0000

    new

--- rust/src/data/data.rs
+++ rust/src/data/data.rs
@@ -3,12 +3,17 @@ use polars::prelude::*;
 pub fn getData() -> Result<(), Box<dyn std::error::Error>> {
     let csv_path = "caminho";
 
-    let data = CsvReader::from_path(csv_path)?
+    let data_train = CsvReader::from_path(../../mnist/train.csv)?
+        .infer_schema(None)
+        .has_header(true)
+        .finish()?;
+
+    let data_test = CsvReader::from_path(../../mnist/train.csv)?
         .infer_schema(None)
         .has_header(true)
         .finish()?;
 
     println!("Data: {:?}", data);
 
-    return data
+    return [data_train, data_test]
 }
