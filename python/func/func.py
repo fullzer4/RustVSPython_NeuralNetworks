4a83e310892f219f2e6ca9dbc31b1d6057cecb94
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-01 09:54:59 -0300

    new

--- python/func/func.py
+++ python/func/func.py
@@ -6,3 +6,61 @@ def init_params():
     W2 = np.random.rand(10, 10) - 0.5
     b2 = np.random.rand(10, 1) - 0.5
     return W1, b1, W2, b2
+
+def RELU(Z):
+    return np.maximum(0, Z)
+
+def deriv_RELU(Z):
+    return np.where(Z >= 0, 1, 0)
+
+def softmax(Z):
+    e_Z = np.exp(Z - np.max(Z))  
+    return e_Z / np.sum(e_Z, axis=0)
+
+def one_hot(Y): #s
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def fof_propagation(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = RELU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def back_propagation(Z1, A1, Z2, A2, W2, X, Y):
+    m = X.shape[1]
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * deriv_RELU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+def update(W1, b1, W2, b2,dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2   
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    return np.sum(predictions == Y) / Y.size
+
+def grad_descent(X, Y, iterations, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = fof_propagation(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_propagation(Z1, A1, Z2, A2, W2, X, Y)
+        W1, b1, W2, b2 = update(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            print("Acurracy: ", get_accuracy(get_predictions(A2), Y))
+    return W1, b1, W2, b2
--- python/script.py
+++ python/script.py
@@ -1,5 +1,5 @@
 from data.data import getdata
-from func.func import init_params
+from func.func import grad_descent, fof_propagation, get_predictions
 import numpy as np
 
 def main():
@@ -19,6 +19,22 @@ def main():
     x_train = dataTrain[1:n]
     y_train = dataTrain[0]
     
-    W1, b1, W2, b2 = init_params()
+    W1, b1, W2, b2 = grad_descent(x_train, y_train, 100, 0.1)
 
+    def make_predictions(X, W1, b1, W2, b2):
+        _, _, _, A2 = fof_propagation(W1, b1, W2, b2, X)
+        predictions = get_predictions(A2)
+        return predictions
+
+    def test_prediction(index, W1, b1, W2, b2):
+        current_image = x_train[:, index, None]
+        prediction = make_predictions(x_train[:, index, None], W1, b1, W2, b2)
+        label = y_train[index]
+        print("Prediction: ", prediction)
+        print("Label: ", label)
+
+    test_prediction(0, W1, b1, W2, b2)
+    test_prediction(1, W1, b1, W2, b2)
+    test_prediction(2, W1, b1, W2, b2)
+    test_prediction(3, W1, b1, W2, b2)
 main()
